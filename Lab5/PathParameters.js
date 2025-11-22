a84add7deca33d4aa4d9b1952aba286d59b2f93c
Vishwapujara <vishwapujara.08@gmail.com>
2025-11-21 23:07:00 -0500

    22th

--- Lab5/PathParameters.js
+++ Lab5/PathParameters.js
@@ -9,6 +9,20 @@ export default function PathParameters(app) {
    const sum = parseInt(a) - parseInt(b);
    res.send(sum.toString());
  };
+ const multiply = (req, res) => {
+   const { a, b } = req.params;
+   const product = parseInt(a) * parseInt(b);
+   res.send(product.toString());
+ };
+ const divide = (req, res) => {
+   const { a, b } = req.params;
+   if (parseInt(b) === 0) {
+     res.status(400).send("Cannot divide by zero");
+     return;
+   }
+   const quotient = parseInt(a) / parseInt(b);
+   res.send(quotient.toString());
+ };
  app.get("/lab5/add/:a/:b", add);
  app.get("/lab5/subtract/:a/:b", substract);
 };
